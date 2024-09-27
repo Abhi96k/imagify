b0ccbdaa9671d3856c28d8cbc2ce0cccbe5093d0
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-27 23:15:49 +0530

    Add Sidebar component and navigation links to layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,13 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <main className="root">
+    {/* <sidebar /> */}
+    {/* <MobileNav /> */}
+
+    <Sidebar />
+
     <div className="root-container">
       <div className="wrapper">{children}</div>
     </div>
