cde5c4bfedf0ecf282ca8f4a635a3e05a2eb5379
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-27 22:21:19 +0530

    Add initial layout, pages, and utility functions for authentication and profile management

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <main className="root">
+    <div className="root-container">
+      <div className="wrapper">{children}</div>
+    </div>
+  </main>
+);
+
+export default Layout;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>
+      <h1>Update Page</h1>
+    </div>
+  );
+};
+
+export default UpdateTransformationPage;
