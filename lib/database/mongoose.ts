23d8a6676f5336176d45d5282bd317f7df10fed5
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-28 01:37:06 +0530

    Refactor database connection utility to use ES2015 module syntax and improve global caching logic

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,32 +7,30 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+// Declare a global object with the type specified inline, using ES2015 module syntax
+const globalCached = global as typeof global & { mongoose: MongooseConnection };
+
+let cached: MongooseConnection = globalCached.mongoose || {
+  conn: null,
+  promise: null,
+};
 
 if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+  cached = globalCached.mongoose = { conn: null, promise: null };
 }
 
-export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+export const connectToDatabase = async (): Promise<Mongoose> => {
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) {
-    throw new Error("Missing MONGODB_URL");
-  }
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
       dbName: "imaginify",
       bufferCommands: false,
     });
+  }
 
   cached.conn = await cached.promise;
-
   return cached.conn;
 };
