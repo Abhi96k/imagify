a34f3572db5b2982f520ec1bce79522e633b0b96
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-28 09:19:33 +0530

    Enhance IImage and User schemas by adding optional fields for better flexibility

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -2,22 +2,35 @@ import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
+
   transformationType: string;
+
   publicId: string;
+
   secureURL: string;
+
   width?: number;
+
   height?: number;
+
   config?: object;
+
   transformationUrl?: string;
+
   aspectRatio?: string;
+
   color?: string;
+
   prompt?: string;
+
   author: {
     _id: string;
     firstName: string;
     lastName: string;
   };
+
   createdAt?: Date;
+
   updatedAt?: Date;
 }
 
