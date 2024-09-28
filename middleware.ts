a1dad418f7509cff4f6fe0e2aaa8026a2c12b276
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-28 10:46:16 +0530

    Remove unused ESLint disable comments from middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-/* eslint-disable no-unused-vars */
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/
 
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
 import { NextRequest } from "next/server";
 
 export default function middleware(req: NextRequest) {
