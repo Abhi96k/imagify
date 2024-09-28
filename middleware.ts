2a6207da75de8f92321fe668ae5cb03b15eac17e
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-28 13:06:50 +0530

    Refactor middleware to use clerkMiddleware for improved authentication handling

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,13 @@
-import { getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default function middleware(req: NextRequest) {
-  const { userId } = getAuth(req);
-
-  const publicRoutes = ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"];
-
-  // If the route is public, allow access
-  if (publicRoutes.includes(req.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-
-  // If there's no user ID, redirect to the sign-in page
-  if (!userId) {
-    return NextResponse.redirect("/sign-in");
-  }
-
-  // Allow access to the protected route
-  return NextResponse.next();
-}
+export default clerkMiddleware({
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+});
 
 export const config = {
   matcher: [
+    // Match routes for the middleware
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
-    "/",
   ],
 };
